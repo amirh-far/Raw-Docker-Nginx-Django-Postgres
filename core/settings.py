2dea97feb30f6afd73dcac89a7f43c391fbd5938
amirh-far <Ahosein54@gamil.com>
2023-10-30 17:00:40 +0330

    postgres connected to django

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,12 @@ ASGI_APPLICATION = 'core.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
